dbce88d0cedf6a78eb9645d017480ea70444273a
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2019-08-14 17:20:56 -0400

    Merge branch 'master' of https://github.com/AltheFlyer/Talk-And-Nothing-Breaks

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public Transform transform;
+    public float rotSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            float x = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+            transform.RotateAround(transform.position, new Vector3(0, -1, 0), x);
+            float y = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+            transform.RotateAround(transform.position, new Vector3(1, 0, 0), y);
+        }
+    }
+
+}
